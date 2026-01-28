38ff24f08ed6b5085a21c908b78a478611bc2c32
Ibtesam Naeem <ibtesamnaeem@gmail.com>
2026-01-28 02:31:36 -0500

    cleaned

---
+++ backend/bouncie/constants.py
@@ -0,0 +1,16 @@
+# ------------------------------ BOUNCIE CONSTANTS ------------------------------
+
+DEFAULT_TIME_BUFFER_HOURS = 2
+
+DEFAULT_DAYS_BACK = 30  
+DEFAULT_SYNC_DAYS_BACK = 365  
+WEBHOOK_TRIP_MATCH_DAYS_BACK = 7 
+CHUNK_DELTA_DAYS = 7  
+
+REQUEST_TIMEOUT_SECONDS = 30  
+DEFAULT_TOKEN_EXPIRY_SECONDS = 3600 
+
+CONTENT_TYPE_JSON = "application/json"
+USER_AGENT = "BouncieAPI/1.0.0"
+
+# ------------------------------ END OF FILE ------------------------------
